e7e4e45b57a1973733f4518d8f1d1805d200402a
TheSithPadawan <miaorr1994@gmail.com>
2024-09-20 21:08:47 -0400

    Added bfs version of cheapeast flight k stop

--- NeatCode/graph/cheapeast_flight_k_stop.py
+++ NeatCode/graph/cheapeast_flight_k_stop.py
@@ -25,4 +25,36 @@ class Solution:
 # lc link: https://leetcode.com/problems/cheapest-flights-within-k-stops/description/
 # modified dijkstra algo. do not visit the current node if exceeds step requirement
 # or we reached the place with lower cost and lower step. lower cost because it is a minheap
-# therefore just need to keep track of the steps 
+# therefore just need to keep track of the steps
+
+
+"""
+BFS solution: Fix step, minimize cost
+""" 
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # BFS: perform k steps from src, and update the min prices 
+        INF = 2 ** 31 - 1
+        adj = defaultdict(list)
+        for a, b, price in flights:
+            adj[a].append((b, price))
+        costs = [INF] * n
+        costs[src] = 0
+        que = deque()
+        que.append((src, 0))
+        seen = set()
+        step = 0
+        while que:
+            for i in range(len(que)):
+                node, cost = que.popleft()
+                if node in seen:
+                    continue
+                if step > k:
+                    continue
+                for nei, p in adj[node]:
+                    if cost + p < costs[nei]:
+                        costs[nei] = cost + p
+                        que.append((nei, costs[nei]))
+            step += 1
+        return -1 if costs[dst] == INF else costs[dst]
+
